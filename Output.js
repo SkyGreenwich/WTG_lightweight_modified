44607ccd81720900d79c0844fb419994087fe1c8
SkyGreenwich <MoJ.live@outlook.com>
2026-04-04 15:55:53 +0800

    Initial commit

---
+++ Output.js
@@ -0,0 +1,12 @@
+// Output Script - WTG Lightweight Modified
+// Derivative of WTG 2.0 by thedenial. - Apache 2.0 - See LICENSE
+
+// Every script needs a modifier function
+const modifier = (text) => {
+  text = onOutput_WTG(text);
+
+  return { text }
+}
+
+// Do not modify this part
+modifier(text)
